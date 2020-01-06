4988b2adec990befc540e7cbdf87d80fedfcade7
eddie.gao <eddie.gao@dji.com>
2020-01-06 22:28:39 +0800

    feat: add custom swiper in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:convert';
+import '../services/service_method.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('商城首页'),
+      appBar: AppBar(title: Text('百姓生活+')),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            return Column(
+              children: <Widget>[
+                CustomSwiper(swiperDataList: swiper),
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中...'),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+// 首页轮播组件
+class CustomSwiper extends StatelessWidget {
+  final List swiperDataList;
+
+  CustomSwiper({Key key, this.swiperDataList}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    // print('设备像素密度: ${ScreenUtil.pixelRatio}');
+    // print('设备高度: ${ScreenUtil.screenHeight}');
+    // print('设备宽度: ${ScreenUtil.screenWidth}');
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(333),
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network('${swiperDataList[index]['image']}', fit: BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
       ),
     );
   }
