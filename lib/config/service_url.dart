5efa2ae93e73b063ab3f7f8c14b47e8aa0537a08
eddie.gao <eddie.gao@dji.com>
2020-01-22 17:45:33 +0800

    feat: category page initial commit

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -2,4 +2,5 @@ const serviceUrl = 'http://v.jspang.com:8088/baixing/';
 const servicePath = {
   'homePageContent': serviceUrl + 'wxmini/homePageContent', // 商城首页信息
   'homePageBelowContent': serviceUrl + 'wxmini/homePageBelowConten', // 商城首页底部热卖商品
+  'getCategory': serviceUrl + 'wxmini/getCategory', // 商品类别信息
 };
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,11 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:convert';
+import '../services/service_method.dart';
+import '../models/category.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('分类页面'),
+      appBar: AppBar(title: Text('商品分类')),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+class LeftCategoryNav extends StatefulWidget {
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  initState() {
+    super.initState();
+    _getCategory();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(width: 1, color: Colors.black12),
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
+      CategoryModel category = CategoryModel.fromJson((data));
+      setState(() {
+        list = category.data;
+      });
+    });
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () { print('点击左侧导航项'); },
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.black12),
+          ),
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
       ),
     );
   }
