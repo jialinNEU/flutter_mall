080c5a3d8ae830f62fc4518e909c0c2ed78026f3
eddie.gao <eddie.gao@dji.com>
2020-02-06 23:25:10 +0800

    feat: add cart page bottom bar

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -15,12 +15,21 @@ class CartPage extends StatelessWidget{
         future: _getCartInfo(context),
         builder: (context, snapshot) {
           List cartList = Provide.value<CartProvide>(context).cartList;
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: cartList.length,
-              itemBuilder: (context, index) {
-                return CartItem(cartList[index]);
-              }
+          if (snapshot.hasData && cartList != null) {
+            return Stack(
+              children: <Widget>[
+                ListView.builder(
+                  itemCount: cartList.length,
+                  itemBuilder: (context, index) {
+                    return CartItem(cartList[index]);
+                  }
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: CartBottom(),
+                ),
+              ],
             );
           } else {
             return Text('正在加载');
@@ -121,6 +130,111 @@ class CartItem extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+
+class CartBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      width: ScreenUtil().setWidth(750),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          _selectAllBtn(),
+          _totalPriceArea(),
+          _caculateBtn(),
+        ],
+      ),
+    );
+  }
+
+  Widget _selectAllBtn() {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Checkbox(
+            value: true,
+            activeColor: Colors.pink,
+            onChanged: (bool val) {},
+          ),
+          Text('全选'),
+        ],
+      ),
+    );
+  }
+
+  Widget _totalPriceArea() {
+    return Container(
+      width: ScreenUtil().setWidth(430),
+      alignment: Alignment.centerRight,
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container(
+                width: ScreenUtil().setWidth(280),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  '合计:',
+                  style:TextStyle(
+                    fontSize: ScreenUtil().setSp(36)
+                  ),
+                ), 
+              ),
+              Container(
+                width: ScreenUtil().setWidth(150),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  '￥1922',
+                  style:TextStyle(
+                    fontSize: ScreenUtil().setSp(36),
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: ScreenUtil().setWidth(430),
+            alignment: Alignment.centerRight,
+            child: Text(
+              '满10元免配送费，预购免配送费',
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: ScreenUtil().setSp(22)
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _caculateBtn() {
+    return Container(
+      width: ScreenUtil().setWidth(160),
+      padding: EdgeInsets.only(left: 10),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+             color: Colors.red,
+             borderRadius: BorderRadius.circular(3.0)
+          ),
+          child: Text(
+            '结算(6)',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+        ),
+      ) ,
     );
   }
 }
